fc9d5643f3f9af8c7fd526af48134fbbe7e1ae2a
trancuong-meth <ttrancuongg2003@gmail.com>
2025-02-25 09:39:16 +0700

    bổ sung thêm việc tự gửi tin nhắn khi chọn các chủ đề

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -12,11 +12,10 @@ const Chatbot = () => {
     const chatEndRef = useRef(null); // Dùng để tự động cuộn đến tin nhắn cuối
     const [isLoading, setIsLoading] = useState(false); // Trạng thái để kiểm soát khi bot đang trả lời
     const [option, setOption] = useState({
-        'apikey': `Bearer ${AppConstants.API_MODEL_BASE}`,
+        'apikey': AppConstants.API_MODEL_BASE,
     },);
     const [display, setDisplay] = useState('true')
 
-    // Hàm chuyển đổi Markdown thành HTML và highlight code
     const parseMarkdown = (text) => {
         const html = marked(text, {
             highlight: function (code, lang) {
@@ -35,16 +34,103 @@ const Chatbot = () => {
         chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
     }, [messages]);
 
-    // Hàm gửi tin nhắn đến server (chatbot)
-    const sendMessage = async () => {
-        if (input.trim() === "") return; // Nếu không có input, không gửi gì
+    // const sendMessage = async () => {
+    //     if (input.trim() === "") return; // Nếu không có input, không gửi gì
+
+    //     setIsLoading(true); // Đánh dấu bắt đầu trạng thái loading
+
+    //     const userMessage = { sender: "user", text: input }; // Tạo đối tượng tin nhắn của người dùng
+
+    //     // Thêm tin nhắn người dùng vào trước khi gửi request
+
+    //     setMessages((prev) => [...prev, userMessage]);
+
+    //     try {
+    //         // Gửi request đến API của chatbot
+    //         const response = await fetch(`${AppConstants.BASE_URL}/chat-messages`, {
+    //             method: "POST",
+    //             headers: {
+
+    //                 'Authorization': option.apikey, // Thay bằng API Key của bạn
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify({
+    //                 "inputs": {},
+    //                 "query": input, // Truyền câu hỏi người dùng gửi vào
+    //                 "response_mode": "streaming", // Cấu hình chế độ phản hồi streaming
+    //                 "conversation_id": "", // Nếu có conversation_id, có thể thêm vào đây
+    //                 "user": "test-react-app", // ID người dùng
+    //             })
+    //         });
+
+    //         setInput(""); // Sau khi gửi xong, làm rỗng input của người dùng
+
+
+    //         const reader = response.body.getReader(); // Đọc phản hồi streaming từ API
+    //         const decoder = new TextDecoder(); // Dùng để giải mã chuỗi bytes thành string
+    //         let accumulatedText = ""; // Biến này lưu trữ văn bản trả lời từ chatbot
+
+    //         let botMessageIndex;
+    //         setMessages((prev) => {
+    //             const newBotMessage = { sender: "bot", text: "" };
+    //             botMessageIndex = prev.length; // Lưu chỉ mục của tin nhắn bot
+    //             return [...prev, newBotMessage];
+    //         });
+
+    //         while (true) {
+    //             const { done, value } = await reader.read(); // Đọc từng chunk
+    //             if (done) break; // Nếu hết dữ liệu, thoát khỏi vòng lặp
+
+    //             const chunk = decoder.decode(value);
+    //             const lines = chunk.split("\n").filter((line) => line.trim() !== "");
+
+    //             lines.forEach((line) => {
+    //                 if (line.startsWith("data: ")) {
+    //                     const jsonString = line.replace("data: ", "");
+
+    //                     try {
+    //                         const jsonData = JSON.parse(jsonString); // Chuyển chuỗi JSON thành đối tượng
+    //                         if (jsonData.answer) {
+    //                             accumulatedText += jsonData.answer;
+
+
+    //                             setMessages((prev) => {
+    //                                 const updatedMessages = [...prev];
+    //                                 updatedMessages[botMessageIndex] = {
+    //                                     ...updatedMessages[botMessageIndex],
+    //                                     text: accumulatedText // Cập nhật text của bot
+    //                                 };
+    //                                 return updatedMessages;
+    //                             });
+    //                         }
+    //                     } catch (error) {
+    //                         console.error("Lỗi khi parse JSON: ", error); // Nếu có lỗi khi parse JSON
+    //                     }
+    //                 }
+    //             });
+    //         }
+    //     } catch (error) {
+    //         console.error("Error:", error); // Xử lý lỗi nếu có
+    //         const errorMessage = { sender: "bot", text: "Xin lỗi, có lỗi xảy ra!" }; // Tin nhắn lỗi từ bot
+    //         setMessages((prev) => [...prev, errorMessage]); // Thêm tin nhắn lỗi vào
+    //     }
+
+    //     setIsLoading(false); // Kết thúc trạng thái loading
+
+    // };
+
+    const sendMessage = async (text_, apikey_) => {
+        const key = apikey_ == null ? option.apikey : apikey_;
+
+        if (input.trim() === "" && text_ == null) return; // Nếu không có input, không gửi gì
+
+        const text = input.trim() === "" ? text_ : input.trim()
 
         setIsLoading(true); // Đánh dấu bắt đầu trạng thái loading
 
-        const userMessage = { sender: "user", text: input }; // Tạo đối tượng tin nhắn của người dùng
+        const userMessage = { sender: "user", text: text }; // Tạo đối tượng tin nhắn của người dùng
 
         // Thêm tin nhắn người dùng vào trước khi gửi request
-
         setMessages((prev) => [...prev, userMessage]);
 
         try {
@@ -52,28 +138,24 @@ const Chatbot = () => {
             const response = await fetch(`${AppConstants.BASE_URL}/chat-messages`, {
                 method: "POST",
                 headers: {
-
-                    'Authorization': option.apikey, // Thay bằng API Key của bạn
+                    'Authorization': key, // Thay bằng API Key của bạn
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     "inputs": {},
-                    "query": input, // Truyền câu hỏi người dùng gửi vào
+                    "query": text, // Truyền câu hỏi người dùng gửi vào
                     "response_mode": "streaming", // Cấu hình chế độ phản hồi streaming
                     "conversation_id": "", // Nếu có conversation_id, có thể thêm vào đây
                     "user": "test-react-app", // ID người dùng
                 })
-
             });
 
             setInput(""); // Sau khi gửi xong, làm rỗng input của người dùng
-
 
             const reader = response.body.getReader(); // Đọc phản hồi streaming từ API
             const decoder = new TextDecoder(); // Dùng để giải mã chuỗi bytes thành string
             let accumulatedText = ""; // Biến này lưu trữ văn bản trả lời từ chatbot
 
-            // Thêm botMessage rỗng vào danh sách tin nhắn, sau đó cập nhật nó sau khi nhận được câu trả lời từ API
             let botMessageIndex;
             setMessages((prev) => {
                 const newBotMessage = { sender: "bot", text: "" };
@@ -81,15 +163,12 @@ const Chatbot = () => {
                 return [...prev, newBotMessage];
             });
 
-            // Đọc từng chunk dữ liệu từ phản hồi streaming
-
-            while (true) {
+            while (text !== "" || text != null) {
                 const { done, value } = await reader.read(); // Đọc từng chunk
                 if (done) break; // Nếu hết dữ liệu, thoát khỏi vòng lặp
 
                 const chunk = decoder.decode(value);
                 const lines = chunk.split("\n").filter((line) => line.trim() !== "");
-
 
                 lines.forEach((line) => {
                     if (line.startsWith("data: ")) {
@@ -100,14 +179,11 @@ const Chatbot = () => {
                             if (jsonData.answer) {
                                 accumulatedText += jsonData.answer;
 
-
                                 setMessages((prev) => {
                                     const updatedMessages = [...prev];
                                     updatedMessages[botMessageIndex] = {
                                         ...updatedMessages[botMessageIndex],
-
                                         text: accumulatedText // Cập nhật text của bot
-
                                     };
                                     return updatedMessages;
                                 });
@@ -125,14 +201,10 @@ const Chatbot = () => {
         }
 
         setIsLoading(false); // Kết thúc trạng thái loading
-
     };
 
-    // Hàm để xử lý khi người dùng chọn một tùy chọn
     const handleOptionSelect = (option) => {
-        // setShowOptions(false); // Ẩn các tùy chọn khi người dùng chọn
-        setOption(option); // Đặt lựa chọn người dùng vào input
-        console.log(option)
+        setOption(option);
         setDisplay("none")
     };
 
@@ -141,7 +213,6 @@ const Chatbot = () => {
     return (
         <div className="chat-container">
             <div className="chat-box">
-
                 {messages.map((msg, index) => (
                     <div key={index} className={`message ${msg.sender}`}>
                         <div
@@ -152,48 +223,50 @@ const Chatbot = () => {
                 ))}
                 <div ref={chatEndRef} />
             </div>
-
             <div className="options" id="option-list" style={{ display: display }}>
                 {options.map((option, index) => (
                     <button
                         key={index}
-                        onClick={() => handleOptionSelect(option)}>
+                        onClick={() => {
+                            handleOptionSelect(option)
+                            sendMessage(`Hãy cho tôi biết về ${option.name}`, option.apikey)
+                        }}>
                         {option.name}
                     </button>
                 ))}
-                <button
-                    onClick={() =>
-                        setDisplay("none")
-                    }>
-                    <span className="material-icons icon-close-option">close</span>
-                </button>
             </div>
-
             <div className="input-container">
                 {isLoading ? (
-                    <div className="loading-indicator">Loading ...</div> // Hiển thị trạng thái loading khi bot đang trả lời
+                    <div className="loading-indicator">
+                        <span>
+                            Loading ...
+                        </span>
+                    </div>
                 ) : (
                     <>
-
                         <textarea
                             value={input}
                             onChange={(e) => setInput(e.target.value)}
-                            onKeyPress={(e) => e.key === "Enter" && !e.shiftKey && sendMessage()}
+                            onKeyPress={(e) => {
+                                e.key === "Enter" && !e.shiftKey && sendMessage()
+                                if (e.key === "Enter" && !e.shiftKey) {
+                                    document.getElementById('option-list').style.display = "none";
+                                }
+                            }}
                             placeholder="Nhập tin nhắn..."
                             style={{
                                 resize: "none",
                                 overflowY: "hidden",
-                                height: "40px",
+                                height: "43px",
                                 maxHeight: "120px",
                             }}
                             onInput={(e) => {
-                                // let option = doccument.get
-                                e.target.style.height = "40px";
+                                e.target.style.height = "43px";
                                 const newHeight = e.target.scrollHeight;
                                 const maxHeight = 120;
                                 e.target.style.height = `${Math.min(newHeight, maxHeight)}px`;
 
-                                if (newHeight <= 40) {
+                                if (newHeight >= 43 && newHeight < 120) {
                                     e.target.style.overflowY = "hidden";
                                 } else {
                                     e.target.style.overflowY = "auto";
@@ -203,7 +276,6 @@ const Chatbot = () => {
                         <button onClick={sendMessage}>
                             <FaPaperPlane />
                         </button>
-
                     </>
                 )}
             </div>
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,14 +1,14 @@
 export default class AppConstants {
     static BASE_URL = "http://localhost/v1";
-    static API_MODEL_BASE = "app-tMD55bdnR0zP2alcE1kr8dfY";
+    static API_MODEL_BASE = "Bearer app-tMD55bdnR0zP2alcE1kr8dfY";
     static API_MODEL_OPTIONS = [
         {
-            'name': 'Dế mèn phiêu lưu kí',
-            'apikey': `Bearer app-tMD55bdnR0zP2alcE1kr8dfY`,
+            'name': 'Công nghệ thông tin',
+            'apikey': `Bearer app-yjA2CFcTA6yqZNJ1kybV6PWC`,
         }
         , {
-            'name': 'Luật giao thông',
-            'apikey': `Bearer app-tMD55bdnR0zP2alcE1kr8dfY`,
+            'name': 'Y tế',
+            'apikey': `Bearer app-oEGgAaAQKdQMwLIzXLKqITfQ`,
         }
     ]
 }
